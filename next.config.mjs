a53d75241e1f3a9e4600f6931c2305242cf3c02a
Axel Urizar <axelurizarmartin@gmail.com>
2024-09-06 13:33:39 +0200

    New Actions Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
 export default nextConfig;
