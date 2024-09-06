978ef64edb426c64414cc2ee1b8219985129b98f
Axel Urizar <axelurizarmartin@gmail.com>
2024-09-06 13:27:25 +0200

    Added github Actions"
    "

--- components/pagina-reservas.tsx
+++ components/pagina-reservas.tsx
@@ -9,7 +9,7 @@ export function PaginaReservas() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-background p-4">
     {/* <h1 className="text-3xl font-bold mb-8 text-center text-black">Sistema de Reservas</h1> */}
       <div className="w-full max-w-md grid grid-cols-1 md:grid-cols-2 gap-6">
-        <Link href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0ZBo10Q6legkGxgkv8TtW60QZzddWtuejEGCTyqKr2LZ9iGPzYDtxzA284bokiOBEOpyxY918r" className="w-full block">
+        <Link href="https://calendar.app.google/uETy1gvvnku23xic9" className="w-full block">
           <Button 
             className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
             variant="default"
@@ -18,7 +18,7 @@ export function PaginaReservas() {
             <span className="text-xl text-center">Reservar Sala Ancianos</span>
           </Button>
         </Link>
-        <Link href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0ZBo10Q6legkGxgkv8TtW60QZzddWtuejEGCTyqKr2LZ9iGPzYDtxzA284bokiOBEOpyxY918r" className="w-full block">
+        <Link href="https://calendar.app.google/H1GmZ8CSwuQ21xM96" className="w-full block">
           <Button 
             className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
             variant="default"
