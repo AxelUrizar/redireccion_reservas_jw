bb0cf3577a1a05b47631b9bbd8304a7cbf8c74b9
Axel Urizar <axelurizarmartin@gmail.com>
2024-11-16 11:48:06 +0100

    New fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Sistema de Reservas",
   description: "Sistema de Reservas para las distintas salas del salón del reino.",
   icons: {
-    icon: "/logo.ico"
+    icon: "/favicon.ico"
   }
 };
 
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <link rel="icon" type="image/png" href="/logo-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-        <link rel="shortcut icon" href="/logo.ico" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
