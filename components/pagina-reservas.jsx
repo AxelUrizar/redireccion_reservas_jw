ee2a28ad43c1dcd1cad111fc5010e148c9234add
Axel Urizar <axelurizarmartin@gmail.com>
2024-11-16 09:58:06 +0100

    Añadido botón de redirección para reservar el auditorio principal

--- components/pagina-reservas.jsx
+++ components/pagina-reservas.jsx
@@ -2,13 +2,21 @@
 
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { Users, BookOpen } from "lucide-react"
+import { Users, BookOpen, Lectern } from "lucide-react"
 
 export function PaginaReservas() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background p-4">
-    {/* <h1 className="text-3xl font-bold mb-8 text-center text-black">Sistema de Reservas</h1> */}
-      <div className="w-full max-w-md grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="w-full max-w-md grid grid-cols-1 gap-6">
+        <Link href="https://calendar.app.google/xN3SVCPmpRXt4G737" className="w-full block">
+          <Button 
+            className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
+            variant="default"
+          >
+            <Lectern className="w-16 h-16" />
+            <span className="text-xl text-center">Reservar Auditorio Principal</span>
+          </Button>
+        </Link>
         <Link href="https://calendar.app.google/uETy1gvvnku23xic9" className="w-full block">
           <Button 
             className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
