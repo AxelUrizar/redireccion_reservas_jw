202f6987f5fde54cd5949eef01d6d7e5d3227cd8
Axel Urizar <axelurizarmartin@gmail.com>
2024-11-17 20:11:48 +0100

    Cambiado el color de los botones a azul y aÃñadido dos botones.

--- components/pagina-reservas.jsx
+++ components/pagina-reservas.jsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { BookOpen } from "lucide-react"
+import { BookOpen, PanelBottomOpen, PanelTopOpen } from "lucide-react"
 
 export function PaginaReservas() {
   return (
@@ -10,11 +10,29 @@ export function PaginaReservas() {
       <div className="w-full max-w-md grid grid-cols-1 gap-6">
         <Link href="https://calendar.app.google/Cq4ZYBsR2GUV5r3q7" className="w-full block">
           <Button 
-            className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
+            className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 bg-sky-700 hover:bg-white hover:text-sky-700 hover:border-sky-700 hover:border-2 transition-colors duration-300 p-6" 
+            variant="default"
+          >
+            <PanelBottomOpen className="w-16 h-16" />
+            <span className="text-xl text-center">Reservar Segunda Sala (Aud. Superior)</span>
+          </Button>
+        </Link>
+        <Link href="https://calendar.app.google/Cq4ZYBsR2GUV5r3q7" className="w-full block">
+          <Button 
+            className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 bg-sky-700 hover:bg-white hover:text-sky-700 hover:border-sky-700 hover:border-2 transition-colors duration-300 p-6" 
             variant="default"
           >
             <BookOpen className="w-16 h-16" />
             <span className="text-xl text-center">Reservar Biblioteca</span>
+          </Button>
+        </Link>
+        <Link href="https://calendar.app.google/Cq4ZYBsR2GUV5r3q7" className="w-full block">
+          <Button 
+            className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 bg-sky-700 hover:bg-white hover:text-sky-700 hover:border-sky-700 hover:border-2 transition-colors duration-300 p-6" 
+            variant="default"
+          >
+            <PanelTopOpen className="w-16 h-16" />
+            <span className="text-xl text-center">Reservar Segunda Sala (Aud. Inferior)</span>
           </Button>
         </Link>
       </div>
