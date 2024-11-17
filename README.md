340d540483a91afa203bea7c23d283a2eef490c9
Axel Urizar <axelurizarmartin@gmail.com>
2024-11-17 15:46:41 +0100

    Configuración para el salón del reino de San Sebastián de los Reyes

---
+++ README.md
@@ -0,0 +1,1 @@
+# Configuración de la aplicación para el salón del reino de San Sebastián de los Reyes.
--- components/pagina-reservas.jsx
+++ components/pagina-reservas.jsx
@@ -2,37 +2,19 @@
 
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { Users, BookOpen, Lectern } from "lucide-react"
+import { BookOpen } from "lucide-react"
 
 export function PaginaReservas() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background p-4">
       <div className="w-full max-w-md grid grid-cols-1 gap-6">
-        <Link href="https://calendar.app.google/xN3SVCPmpRXt4G737" className="w-full block">
-          <Button 
-            className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
-            variant="default"
-          >
-            <Lectern className="w-16 h-16" />
-            <span className="text-xl text-center">Reservar Auditorio Principal</span>
-          </Button>
-        </Link>
-        <Link href="https://calendar.app.google/uETy1gvvnku23xic9" className="w-full block">
-          <Button 
-            className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
-            variant="default"
-          >
-            <Users className="w-16 h-16" />
-            <span className="text-xl text-center">Reservar Sala Ancianos</span>
-          </Button>
-        </Link>
-        <Link href="https://calendar.app.google/H1GmZ8CSwuQ21xM96" className="w-full block">
+        <Link href="https://calendar.app.google/Cq4ZYBsR2GUV5r3q7" className="w-full block">
           <Button 
             className="w-full h-48 md:h-56 flex flex-col items-center justify-center space-y-4 hover:bg-white hover:text-black hover:border-black hover:border-2 transition-colors duration-300 p-6" 
             variant="default"
           >
             <BookOpen className="w-16 h-16" />
-            <span className="text-xl text-center">Reservar Segunda Sala</span>
+            <span className="text-xl text-center">Reservar Biblioteca</span>
           </Button>
         </Link>
       </div>
